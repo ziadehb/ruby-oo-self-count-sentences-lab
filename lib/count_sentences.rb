970655913b53862036cdc44e362d0b1ac167814c
ziadehb <40444656+ziadehb@users.noreply.github.com>
2020-12-19 22:47:19 -0500

    Add files via upload

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = 0
+    number = 0
+    string_to_array = self.split("")
+    string_to_array.each do |letter|
+      if (string_to_array[number - 1] != letter) && (letter == "." || letter == "?" || letter == "!")
+        count += 1
+      end
+            
+      number += 1
+    end
+          
+    count
   end
+
 end
